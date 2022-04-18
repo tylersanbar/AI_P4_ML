a60c82e975011dad661445b782bce070e6a0f7e8
Tyler Sanbar <tylersanbar@gmail.com>
2022-04-18 07:07:35 -0500

    Q1 sort of done just parameter tweak

--- Q1_DigitClass/models.py
+++ Q1_DigitClass/models.py
@@ -20,6 +20,32 @@ class DigitClassificationModel(object):
     def __init__(self):
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        #Input 784d array
+        #Output 10d array representing 0-9 digits, 0 for all except 1 for classification
+        #98% validation accuracy 
+        feature_size = 784
+        output_size = 10
+        #dataset.get_validation_accuracy()
+        #Hidden Layer Size - (10, 400)
+        hidden_layer_size = 150
+        self.num_hidden_layers = 2
+        #Batch Size - (1, dataset size) 
+        self.batch_size = 100
+        #Learning Rate - (0.001, 1.0)
+        self.alpha = .005
+
+        self.hidden_layers = []
+        for layer in range(self.num_hidden_layers):
+            if layer == 0:
+                W = nn.Parameter(feature_size, hidden_layer_size)
+                b = nn.Parameter(1, hidden_layer_size)
+            else:
+                W = nn.Parameter(hidden_layer_size, hidden_layer_size)
+                b = nn.Parameter(1, hidden_layer_size)
+            self.hidden_layers.append([W, b])
+        self.output_w = nn.Parameter(hidden_layer_size, output_size)
+        self.output_b = nn.Parameter(1, output_size)
+        #Number of hidden layers - (1, 3)
 
     def run(self, x):
         """
@@ -36,6 +62,28 @@ class DigitClassificationModel(object):
                 (also called logits)
         """
         "*** YOUR CODE HERE ***"
+        #f(x)=relu(x⋅W1+b1)⋅W2+b2
+        def hiddenRelu(layer, x):
+            W = self.hidden_layers[layer][0]
+            b = self.hidden_layers[layer][1]
+            #x⋅W
+            x_W = nn.Linear(x, W)
+            #x⋅W+b
+            add_b = nn.AddBias(x_W, b)
+            #relu(x⋅W+b)
+            relu = nn.ReLU(add_b)
+            layer += 1
+            if layer == self.num_hidden_layers: return relu
+            else: return hiddenRelu(layer, relu)
+
+        relu = hiddenRelu(0, x)
+        #relu(x⋅W1+b1)⋅W2
+        times_output_W = nn.Linear(relu, self.output_w)
+        #relu(x⋅W1+b1)⋅W2+b2
+        logits = nn.AddBias(times_output_W, self.output_b)
+
+        return logits
+
 
     def get_loss(self, x, y):
         """
@@ -51,11 +99,35 @@ class DigitClassificationModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
+        loss = nn.SoftmaxLoss(self.run(x), y)
+        #print(nn.as_scalar(loss))
+        return loss
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        for example in dataset.iterate_forever(self.batch_size):
+            x = example[0]
+            y = example[1]
+            if dataset.get_validation_accuracy() > .98: break
+            loss = self.get_loss(x, y)
+            params = []
+            for layer in range(self.num_hidden_layers):
+                params.append(self.hidden_layers[layer][0])
+                params.append(self.hidden_layers[layer][1])
+            params.append(self.output_w)
+            params.append(self.output_b)
+            grads = nn.gradients(loss, params)
+            #I negate the learning rate instead of the gradient direction
+            for layer in range(self.num_hidden_layers):
+                self.hidden_layers[layer][0].update(grads[layer * 2], -1 * self.alpha)
+                self.hidden_layers[layer][1].update(grads[layer * 2 + 1], -1 * self.alpha)
+            self.output_w.update(grads[-2], -1 * self.alpha)
+            self.output_b.update(grads[-1], -1 * self.alpha)
+
+
+
 
 
